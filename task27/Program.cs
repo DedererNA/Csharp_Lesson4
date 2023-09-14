b60b0b0750079c2462a662693cd0d52a93ab27b9
Nikolay Dederer <cmrddrr@gmail.com>
2023-09-14 14:21:26 +0300

    task27 finished

---
+++ task27/Program.cs
@@ -0,0 +1,14 @@
+﻿int GetSumm (int num)
+{
+    int res = 0;
+    while (num>0)
+    {
+        res += num%10;
+        num/=10;
+    }
+    return res;
+}
+
+Console.WriteLine("Введите число");
+int number = GetSumm(Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine($"{number}");
