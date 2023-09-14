dad44e971119d2e1432edb4faa72a3d1c58b9b15
Nikolay Dederer <cmrddrr@gmail.com>
2023-09-14 14:34:25 +0300

    task29 finished

---
+++ task29/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] RandArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    for (int i=0; i<size; i++)
+    {
+        arr[i] = new Random().Next(min, max+1);
+    }
+    return arr;
+}
+
+int ArrSize=8;
+int ArrRanMin=-10;
+int ArrRanMax=10;
+
+int[] array = RandArray(ArrSize, ArrRanMin, ArrRanMax);
+Console.WriteLine(string.Join(" ", array));
