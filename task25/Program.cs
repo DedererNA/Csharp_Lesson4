fbabb819e340b1342ba69fc93266443f39a5fcd4
Nikolay Dederer <cmrddrr@gmail.com>
2023-09-14 14:03:55 +0300

    task25 finished

---
+++ task25/Program.cs
@@ -0,0 +1,18 @@
+﻿double PowNum ()
+{
+    Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите степень");
+    int pow = Convert.ToInt32(Console.ReadLine());
+    while (pow<0)
+    {
+        Console.WriteLine("Недопустимая степень");
+        Console.WriteLine("Введите степень");
+        pow = Convert.ToInt32(Console.ReadLine());
+    }
+    double res = Math.Pow(num, pow);
+    return res;
+}
+
+double result = PowNum();
+Console.WriteLine($"Искомое число: {result}");
